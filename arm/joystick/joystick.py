8e25b3048c5c266bee26a3cfb1f9ef77c897c9c9
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-08-04 23:45:43 +0300

    Update joystick.py

--- arm/joystick/joystick.py
+++ arm/joystick/joystick.py
@@ -1,56 +1,81 @@
 import pygame
+from time import sleep
 
 pygame.init()
-
 pygame.display.set_caption("Joystick")
-
-clock = pygame.time.Clock()
- 
-# Initialize the joysticks
 pygame.joystick.init()
-    
-while True:
-    # EVENT PROCESSING STEP
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done=True # Flag that we are done so we exit this loop
-        
-    # Get count of joysticks
+
+def update(pygame):
+    pygame.event.get()
     joystick_count = pygame.joystick.get_count()
- 
     
     # For each joystick:
     for i in range(joystick_count):
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
-    
-        # Get the name from the OS for the controller/joystick
-        name = joystick.get_name()
         
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
         axes = joystick.get_numaxes()
-        
         for i in range( axes ):
             axis = joystick.get_axis( i )
             if (axis!=0): print("joystick",["Left_X","Left_Y","Right_Y","Right_X"][i], axis)
             
         buttons = joystick.get_numbuttons()
- 
         for i in range( buttons ):
             button = joystick.get_button( i )
             if (button!=0): print("button",i)
             
-        # Hat switch. All or nothing for direction, not like joysticks.
-        # Value comes back in an array.
         hats = joystick.get_numhats()
- 
         for i in range( hats ):
             hat = joystick.get_hat( i )
             if (hat!=(0,0)): print("arrows",i,hat)
-    clock.tick(20)
+            
+
+class my_universal_joystick:
     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit ()
+    def __init__(self):
+        #self.pygame = pygame
+        pass
+        
+    def update(self):
+        pygame.event.get()
+        joystick_count = pygame.joystick.get_count()
+        
+        for i in range(joystick_count):
+            joystick = pygame.joystick.Joystick(i)
+            joystick.init()
+            
+            axes = joystick.get_numaxes()
+            for i in range( axes ):
+                axis = joystick.get_axis( i )
+                if (axis!=0): print("joystick",["Left_X","Left_Y","Right_Y","Right_X"][i], axis)
+                
+            buttons = joystick.get_numbuttons()
+            for i in range( buttons ):
+                button = joystick.get_button( i )
+                if (button!=0): print("button",i)
+                
+            hats = joystick.get_numhats()
+            for i in range( hats ):
+                hat = joystick.get_hat( i )
+                if (hat!=(0,0)): print("arrows",i,hat)
+                
+    def destroy(self):
+        pygame.quit()
+            
+while True:
+    update(pygame)
+    
+    sleep(0.1)
+    
+pygame.quit()
+
+
+if __name__ == "__main__" and 0:
+    test = my_universal_joystick()
+    while True:
+        pygame.event.get()
+        joystick_count = pygame.joystick.get_count()
+        update(joystick_count)
+        sleep(0.1)
+        #print()
+    test.destroy()
