34afccbfe69a87d8213cddae56b1a6149a047c59
Ni3nayka <53381511+Ni3nayka@users.noreply.github.com>
2021-08-04 23:45:41 +0300

    Create easy_joystick.py

---
+++ arm/joystick/easy_joystick.py
@@ -0,0 +1,35 @@
+import pygame
+from time import sleep
+
+pygame.init()
+pygame.display.set_caption("Joystick")
+pygame.joystick.init()
+    
+while True:
+    pygame.event.get()
+    joystick_count = pygame.joystick.get_count()
+ 
+    
+    # For each joystick:
+    for i in range(joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+        
+        axes = joystick.get_numaxes()
+        for i in range( axes ):
+            axis = joystick.get_axis( i )
+            if (axis!=0): print("joystick",["Left_X","Left_Y","Right_Y","Right_X"][i], axis)
+            
+        buttons = joystick.get_numbuttons()
+        for i in range( buttons ):
+            button = joystick.get_button( i )
+            if (button!=0): print("button",i)
+            
+        hats = joystick.get_numhats()
+        for i in range( hats ):
+            hat = joystick.get_hat( i )
+            if (hat!=(0,0)): print("arrows",i,hat)
+            
+    sleep(0.1)
+    
+pygame.quit()
