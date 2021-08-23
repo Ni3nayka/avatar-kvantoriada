793e4778560f384d9da9e4c14bc1fa6c4fa9b217
Egor Bakay <egor_bakay@inbox.ru>
2021-08-24 00:07:48 +0300

    LEVEL COMPLETE!!! ANSWER

--- arm/arduino_bluetooth/arduino_bluetooth.ino
+++ arm/arduino_bluetooth/arduino_bluetooth.ino
@@ -1,30 +1,36 @@
 #define mut_R_pin A0
 #define joystick_x_pin A1
 #define joystick_y_pin A2
-#define joystick_button_pin 4
+#define joystick_button_pin 3
 
 #include "akselerometr.h"
 
 // work const
 #define delta_t 200
+#define motor 10
 
 // working variable
 unsigned long int time;
+bool button_flag = 0;
+bool button_history = 0;
 
 void setup() {
   // setup com port
   Serial.begin(9600);
   // setup digital pin
   pinMode(joystick_button_pin,INPUT_PULLUP);
+  pinMode(motor,OUTPUT);
   // setup akselerometr - i2c
   setup_akselerometr();
 }
 
 void loop() {
-  /*Serial.print(analogRead(A1));
-  Serial.print(" ");
-  Serial.println(analogRead(A2));
-  delay(100);*/
+  if (button_history && !digitalRead(joystick_button_pin)) {
+    button_flag = !button_flag;
+    digitalWrite(motor,button_flag);
+    //delay(100);
+  }
+  button_history = digitalRead(joystick_button_pin);
   if (Serial.available()) {
     Serial.read();
     // elbow        
@@ -32,7 +38,7 @@ void loop() {
     // joystick
     Serial.print(analogRead(joystick_x_pin)); Serial.print(" ");
     Serial.print(analogRead(joystick_y_pin)); Serial.print(" "); 
-    Serial.print(!digitalRead(joystick_button_pin)); Serial.print(" "); 
+    Serial.print(button_flag); Serial.print(" "); 
     // akselerometr
     int angle_ax, angle_ay;
     read_akselerometr(1,angle_ax,angle_ay);
