93c1b4b15008b4b485e33ca5ad1c07579615eea8
Egor Bakay <egor_bakay@inbox.ru>
2021-10-24 20:03:24 +0300

    +

--- src/client/main/client.py
+++ src/client/main/client.py
@@ -1,14 +1,30 @@
 import socket               # Import socket module
 
-s = socket.socket()         # Create a socket object
-#host = "192.168.1.62" #socket.gethostname() # Get local machine name
-host = "LAPTOP-V2VL8AV1"
-port = 12345                # Reserve a port for your service.
 
-try:
-    s.connect((host, port))     
-    for i in range(10):
-        s.send(b'1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20')
-        print(s.recv(1024))
-    s.close                     # Close the socket when done    
-except ConnectionRefusedError or ConnectionResetError: print("connect error") 
+class my_client():
+
+    def __init__(self):
+        self.s = socket.socket()         # Create a socket object
+        self.host = "192.168.43.225" #socket.gethostname() #"192.168.43.225" #socket.gethostname() # Get local machine name
+        #self.host = "raspberrypi" #"LAPTOP-V2VL8AV1"
+        self.port = 12345                # Reserve a port for your service.
+
+    def send(self, message=[[90,90,90,90,90,90,90,90,90,90],[0,0,0,0,0,0,0,0,0,0]]):
+        try:
+            self.s.send(b'45 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90 90')
+            print(self.s.recv(1024)) # print(s.recv(1024))
+        except ConnectionRefusedError or ConnectionResetError or OSError: 
+            print("error connect")
+
+    def connect(self):
+        self.s.connect((self.host, self.port))
+
+    def disconnect(self):
+        self.s.close
+
+if __name__ == "__main__":
+
+    client = my_client()
+    client.connect()
+    client.send([[90,90,90,90,90,90,90,90,90,90],[0,0,0,0,0,0,0,0,0,0]])
+    client.disconnect()
--- src/server/main/main.py
+++ src/server/main/main.py
@@ -2,6 +2,7 @@ from I2C import arduino_i2c
 from server import my_server
 from copy import deepcopy
 from time import sleep
+from camera import my_camera
 
 if __name__ == "__main__":
     server = my_server() 
@@ -9,9 +10,19 @@ if __name__ == "__main__":
     
     arduino = arduino_i2c(0x04)
     
+    camera = my_camera(600)
+    camera.start()
+    
+    mas = [90,90,90,90,45,0,0,0,0,0]
+    
     while True:
-        for i in range (5): arduino.write(i,server.array[0][i])
-        sleep(0.5)
+        for i in range (5):
+            if (mas[i]!=server.array[0][i]):
+                mas[i] = server.array[0][i]
+                arduino.write(i,server.array[0][i])
+        #sleep(0.5)
+        
+    camera.enable = False
     
     
     
